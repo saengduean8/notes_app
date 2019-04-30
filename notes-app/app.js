12818d8499877fa4c185f712b07c325aae9a444e
saengdueancalderaz <happygoldfish8@gmail.com>
2019-04-30 12:12:47 -0700

    initial commit

---
+++ notes-app/app.js
@@ -0,0 +1,5 @@
+const getNotes = require('./notes.js')
+
+const notes = getNotes()
+
+console.log(notes)
---
+++ notes-app/notes.js
@@ -0,0 +1,5 @@
+const getNotes = function() {
+    return 'My note.....'
+}
+
+module.exports = getNotes
