a783ef43729599f93efc85335db5884c30af5580
saengdueancalderaz <happygoldfish8@gmail.com>
2019-04-30 14:32:19 -0700

    Install npm package: validator

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const notes = getNotes()
 
 console.log(notes)
+
+console.log(validator.isEmail('andrew@example.com'))
