7fbe522780f7d1261583ccd1a8ef8db089cf09d2
saengdueancalderaz <happygoldfish8@gmail.com>
2019-05-16 10:45:03 -0700

    completed Add note function

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,55 @@
-const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const notes = getNotes()
+//customize yargs version
+//yargs.version('1.1.0')
 
-console.log(notes)
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
 
-console.log(validator.isEmail('andrew@example.com'))
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note!')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List your note',
+    handler: function(){
+        console.log('Listing out all the notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('Reading a note')
+    }
+})
+
+//console.log(yargs.argv)
+yargs.parse()
